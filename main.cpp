df6b066d603fb05dc471e5d097b4067be05890bc
kadir <eersooy61@gmail.com>
2021-01-04 00:13:07 +0300

    finalversion

--- main.cpp
+++ main.cpp
@@ -7,103 +7,52 @@
 #include <algorithm>
 using namespace std;
 
-// int findVertexID(list<int> *eulerianCircuit,unordered_map<int,*Vertex> *graph);
 
 int main(int argc, char const *argv[]) {
 
-    string infile_name = argv[1];//READ INPUT FILE NAME
-    string outfile_name = argv[2];//READ OUTPUT FILE NAME
+    const char* infile_name = argv[1];//READ INPUT FILE NAME
+    const char* outfile_name = argv[2];//READ OUTPUT FILE NAME
 
     //OPEN INPUT AND OUTPUT FILE STREAMS
-    ifstream infile;
-    infile.open(infile_name);
-    ofstream outfile;
-    outfile.open(outfile_name);
+    FILE * infile;
+    FILE * outfile;
+    infile = fopen(infile_name,"r");
+    outfile = fopen(outfile_name,"w");
+
 
     int numOfVertices=0;
-    infile >> numOfVertices;
-    //cout << numOfVertices<<endl;
-    //int* vertexArr = new int[numOfVertices];// array of zeros
+    fscanf(infile,"%i", &numOfVertices);
     int totalNumOfEdges=0;
     int balance = 0;
     queue<int> * queArr = new queue<int>[numOfVertices];
-    //vector<queue<int>> graphV(numOfVertices);
-
-    //vector<Vertex*> graph;
     for (int i = 0; i < numOfVertices; ++i)
     {
         int vertexID,outDegree;
-        infile >> vertexID >> outDegree;
+        fscanf(infile,"%i",&vertexID);
+        fscanf(infile,"%i",&outDegree);
         totalNumOfEdges+=outDegree;
-        //vertexArr[vertexID]+=outDegree;// for easy calculation of indegree outdegree balance;
-        //Vertex* v = new Vertex(vertexID);
         balance+=vertexID*outDegree;
         for (int j = 0; j < outDegree; ++j)//add edges to vertices, creating graph
         {
             int endVertexID;
-            infile >> endVertexID;
-            //vertexArr[endVertexID]--;// incoming edge
-            //v->addEdge(endVertexID);
+            fscanf(infile,"%i",&endVertexID);
             balance-=endVertexID;
             queArr[i].push(endVertexID);
         }
-        //graph.push_back(v);
     }
     cout << balance;
     int startVertexID;
-    infile >> startVertexID;
-//    for (int i = 0; i < numOfVertices; ++i)
-//    {
-//        cout <<graph[i]->outGoingEdges.size();
-//    }
-//    cout <<  endl;
-    // for(auto v : graphV){
-    //     for(auto e : v){
-    //         cout << e <<  " ";
-    //     }
-    //     cout << endl;
-    // }
-    //cout << "heree" << endl;
-    //bool isDegreesBalanced = true;
+    fscanf(infile,"%i",&startVertexID);
 
-    // for (int i = 0; i < numOfVertices; ++i)
-    // {
-    //     if(vertexArr[i]!=0){
-    //         isDegreesBalanced = false;
-    //         break;
-    //     }
-    // }
-    //cout << isDegreesBalanced << endl;
-
-    //cout << graph.at(0)->outGoingEdges.top();
-    if (balance ==0){
-        // for (int i = 0; i < numOfVertices; ++i)
-        // {
-        //     sort(graph[i]->voutGoingEdges.begin(),graph[i]->voutGoingEdges.end());
-        // }
-        // for (int i = 0; i < numOfVertices; ++i)
-        // {
-        //     vector<int> v = graph.at(i)->voutGoingEdges;
-        //     for(auto x : v){
-        //         outfile << x << " ";
-        //     }
-        //     outfile << endl;
-        // }
+    if (balance !=0){
+        fprintf(outfile, "no path");
+    }
+    else{
         list<int> eulerianCircuit;
-        //int currentV = startVertexID;
-        //int startVindex=0;
         eulerianCircuit.push_back(startVertexID);
         auto startingViterator = eulerianCircuit.begin();
         startingViterator++;
-        // for(auto vId : eulerianCircuit){
-        //     if(vId==currentV){
-        //         startingViterator++;
-        //         break;
-        //     }
-        //     startingViterator++;
-        // }
         auto mockIterator = eulerianCircuit.begin();
-        //mockIterator++;
         while(eulerianCircuit.size() <= totalNumOfEdges){
             list<int> tour;
             while(!queArr[startVertexID].empty()){
@@ -112,17 +61,7 @@ int main(int argc, char const *argv[]) {
                 startVertexID = endVertexID;
                 tour.push_back(startVertexID);
             }
-
-            //auto it = eulerianCircuit.end();
-            // for (auto x : tour)
-            // {
-            //     cout << x << " ";
-            // }
             eulerianCircuit.splice(startingViterator,tour);// how this works ???
-            //auto it = eulerianCircuit.begin();
-            //int newStartingVertex=currentV;
-            //bool found = false;
-            //bool f2 = false;
             while(mockIterator!=eulerianCircuit.end()){
                 if(!queArr[*mockIterator].empty()){
                     startVertexID = *mockIterator;
@@ -131,48 +70,15 @@ int main(int argc, char const *argv[]) {
                 }
                 mockIterator++;
             }
-            // for(auto vID : eulerianCircuit){
-            //     if(graph[vID]->hasNonUsedEdge()){
-            //         startVertexID = vID;
-            //         it++;
-            //         break;
-            //     }
-            //     it++;
-            // }
-            //currentV = newStartingVertex;
             startingViterator = mockIterator;
             mockIterator--;
-            //cout << "heree" << endl;
-            //startVertexID = findVertexID(&eulerianCircuit,&graph);
-            //int index = 0;
-            // for(auto vertexID : eulerianCircuit){
-            //     if(graph.at(vertexID)->hasNonUsedEdge()){
-            //         currentV = vertexID;
-            //         break;
-            //     }
-            //         startVindex++;
-            //     //index++;
-            // }
         }
         //print euleriancircuit
         for(auto ID : eulerianCircuit){
-            outfile << ID << " ";
+            fprintf(outfile,"%i ",ID);
         }
     }
-    else{
-        //print empty circuit
-        outfile << "no path" << endl;
-    }
+    
 
     return 0;
 }
-// int findVertexID(list<int> *eulerianCircuit,unordered_map<int,*Vertex> *graph){//find first vertex in the circuit with unused edge
-//  for(const auto& vertexID : *eulerianCircuit)
-//  {
-//          if(graph[startVertexID]->hasNonUsedEdge()){
-//              return vertexID;
-//          }
-
-//  }
-
-// }
